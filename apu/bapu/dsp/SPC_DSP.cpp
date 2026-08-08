121823cf7a0c399fec212c71d23e420c39e8749a
Carlos Rodrigues <cefrodrigues@gmail.com>
2026-08-09 00:29:39 +0100

    Revert "Merge pull request #303 from eadmaster/master"
    This reverts commit 2c0bb01700e8d51cf7a1eaba15397b115a7edb8e, reversing
    changes made to 185488cd83aaf274752a742c94d45561cbecb7af.

--- apu/bapu/dsp/SPC_DSP.cpp
+++ apu/bapu/dsp/SPC_DSP.cpp
@@ -868,9 +868,7 @@ inline void SPC_DSP::voice_output( voice_t const* v, int ch )
 	// Apply user-set volume (if set)
 	if (Settings.ChannelsVolumePercent[v->voice_number] < 100)
 	{
-		//amp = amp * Settings.ChannelsVolumePercent[v->voice_number] / 100;  // slower
-		// 16384 / 100 is approx 163.84.
-		amp = (amp * Settings.ChannelsVolumePercent[v->voice_number] * 164) >> 14;
+	    amp = amp * Settings.ChannelsVolumePercent[v->voice_number] / 100;
 	}
 	#endif
 	
